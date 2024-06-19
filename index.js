a70ba433ec831e67792a321bed0d58434984191e
Miki-Palmisano <palmmiki21@gmail.com>
2024-06-19 12:35:09 +0200

    API_KEY

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
+const collectionFilms = require('./routes/films')
+const collectionSeries = require('./routes/series')
+
+const app = express();
+
+app.use(express.json());
+app.use('/films', collectionFilms)
+app.use('/series', collectionSeries)
+
+app.listen(3001, () => {
+    console.log("Service Content listening on port 3001")
+})
