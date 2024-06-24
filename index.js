887a5e1a9497c06ee55c97c7c7673316ae786f97
Miki-Palmisano <palmmiki21@gmail.com>
2024-06-24 17:23:51 +0200

    Fixing

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use('/films', collectionFilms)
 app.use('/series', collectionSeries)
 
-app.listen(3001, () => {
-    console.log(`Service Content listening on port 3001`)
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Service Content listening on port ${PORT}`)
 })
