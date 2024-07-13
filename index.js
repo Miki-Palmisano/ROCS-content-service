3378007b06db2edc34e2a46c78f14f0e0a276f15
Miki-Palmisano <palmmiki21@gmail.com>
2024-07-13 13:27:12 +0200

    State Update

--- index.js
+++ index.js
@@ -18,8 +18,16 @@ app.use(express.json());
 app.use(cors(corsOption));
 
 app.use('/state', (req, res) => {
-    console.log('Server is running')
-    res.status(200).json({ message: 'Server is running' });
+    axios.get('https://api.themoviedb.org/3/discover/movie', {
+        headers: {
+            Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+        }
+    }).then(response => {
+        console.log('Server is running');
+        res.status(200).json({ message: 'Server is running', data: response.data });
+    }).catch(error => {
+        res.status(500).json({ message: 'Server is not running', error: error });
+    });
 });
 
 app.use('/films', collectionFilms)
